623eeb17e157e4bc0a4e8b6f82d3d33e7eca3df9
PeterQuinn925 <peterquinn925@gmail.com>
2020-11-28 09:20:52 -0800

    Change Print() for Python3

--- extra_temp.py
+++ extra_temp.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+#updated for Python 3. Just update the print statement
 import socket
 from shutil import copyfile
 
-UDP_IP = "10.0.0.20"
+#Raspi2b is 10.0.0.20, Raspi3 is 10.0.0.30
+#UDP_IP = "10.0.0.20"
+UDP_IP = "10.0.0.30"
 UDP_PORT = 1025
 fname = "/var/tmp/extra_temp.txt"
 
@@ -13,7 +16,7 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    print ("received message:", data)
     f = open(fname,'w')
     f.write(data)
     f.write('\n')
